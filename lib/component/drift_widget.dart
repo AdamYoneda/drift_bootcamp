bcc14e9ce62f9a72abd117a7d89c94ee9028551b
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2025-02-22 18:41:25 +0900

    feat: UI

---
+++ lib/component/drift_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Drift extends StatelessWidget {
+  const Drift({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(child: Container()),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: ElevatedButton(
+                      child: const Text('Add'),
+                      onPressed: () async {},
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: ElevatedButton(
+                      child: const Text('remove'),
+                      onPressed: () async {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
