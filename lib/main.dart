a3a894a0339993af52065fa6a94e06aa34bb3e86
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2025-02-23 01:23:23 +0900

    update: database.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:drift_bootcamp/component/drift_widget.dart';
+import 'package:drift_bootcamp/infrastructure/database.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  final database = MyDatabase();
   runApp(const MyApp());
 }
 
